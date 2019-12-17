9f811158897a402939340b8d400716e08382994f
sanjeev <sanjeevan@nltvc.com>
2019-12-17 20:15:45 +0000

    Finished static design of user info page and main game page

--- codeplay-app/src/components/Game/Game.jsx
+++ codeplay-app/src/components/Game/Game.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { changeView } from "../../actions";
 import AceEditor from "react-ace";
-import "brace/mode/javascript";
-import "brace/theme/monokai";
+import "ace-builds/src-noconflict/mode-typescript";
+import "ace-builds/src-noconflict/theme-monokai";
+import './Game.css';
 
 function Game() {
   const dispatch = useDispatch();
@@ -13,7 +14,107 @@ function Game() {
   }
 
   return (
-    <div>
+    <div className="gamepage">
+      <div className="chapters">
+        <div className="chapterholder">
+          <p>First program</p>
+        </div>
+        <div className="chapterholder">
+          <p>Type annotation</p>
+        </div>
+        <div className="chapterholder">
+          <p>Variables</p>
+        </div>
+        <div className="chapterholder">
+          <p>Numbers</p>
+        </div>
+        <div className="chapterholder">
+          <p>Strings</p>
+        </div>
+        <div className="chapterholder">
+          <p>Boolean</p>
+        </div>
+        <div className="chapterholder">
+          <p>Array</p>
+        </div>
+        <div className="chapterholder">
+          <p>Tuple</p>
+        </div>
+        <div className="chapterholder">
+          <p>Enum</p>
+        </div>
+        <div className="chapterholder">
+          <p>Union</p>
+        </div>
+        <div className="chapterholder current">
+          <p>Any</p>
+        </div>
+        <div className="chapterholder">
+          <p>Void</p>
+        </div>
+        <div className="chapterholder">
+          <p>Never</p>
+        </div>
+        <div className="chapterholder">
+          <p>Type inference</p>
+        </div>
+      </div>
+      <div className="logo">
+        <h3>Code & Play</h3>
+      </div>
+      <div className="userinfotag">
+        <button className="userinfo">User info</button>
+      </div>
+      <div className="story">
+        <p className="storytext" align="justify">One day, the evil Mephisto burned down all of the villages. Everyone died a fiery death but one. The name was Ze. The legend begins...
+        <br></br><br></br>
+        24 years later, Prognosis is still under the wicked rule of Mephisto, or as his friends call him, the Mephman, Phisty Boi or simply, the Big M. Not that he had any friends, no. His friends had died in a freak sleepover accident. Mephisto was the only survivor, and he vowed never to have friends again. He's actually a really misunderstood character. But he did not handle this tragic loss well as he made it his life goal to rule Prognosis, for eternity.
+        <br></br><br></br>
+        Crimbi, the local fool of the village of Gadad, was up to his usual tomfoolery. Being the only fool in the village, he was given special permission to smile and laugh, which was strictly banned for everyone else in Prognosis. Today, Crimbi decided to swim across the river Manto, despite its strong currents and history of drowning swimmers. Yes, Crimbi was a fool alright.
+        </p>
+      </div>
+      <div className="instructionssolution">
+        <p className="instructions" align="left">
+          <ol>
+            <li>Help swim</li>
+            <li>Don't die</li>
+            <li>Don't die</li>
+            <li>Don't die</li>
+            <li>Don't die</li>
+            <li>Don't die</li>
+          
+          </ol>
+        </p>
+        <button className="solutiontag">Solution</button>
+      </div>
+      <div className="code">
+      <AceEditor
+        mode="typescript"
+        theme="monokai"
+        name="code"
+        fontSize={15}
+        height="280px"
+        width="560px"
+        onChange={onChange}
+        name="UNIQUE_ID_OF_DIV"
+        editorProps={{ $blockScrolling: true }}
+      />
+      <div className="codebuttons">
+        <button className="codebutton">Run</button>
+        <button className="codebutton">Restart</button>
+      </div>
+      </div>
+      <div className="output">
+        <p>Hello world</p>
+      </div>
+    </div>
+  );
+}
+export default Game;
+
+
+
+{/* <div>
       {" "}
       <button
         onClick={() => {
@@ -37,7 +138,4 @@ function Game() {
         name="UNIQUE_ID_OF_DIV"
         editorProps={{ $blockScrolling: true }}
       />
-    </div>
-  );
-}
-export default Game;
+      </div> */}
--- codeplay-app/src/components/UserInfo/UserInfo.jsx
+++ codeplay-app/src/components/UserInfo/UserInfo.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 import { changeView, changeAvatar } from "../../actions/index";
+import "./UserInfo.css";
 import axios from "axios";
 
 function UserInfo() {
@@ -23,8 +24,41 @@ function UserInfo() {
   };
 
   return (
-    <div>
-      <button
+    <div className = "userinfopage">
+      <div className = "avatarholder">
+        <img src="troll.png" className="avatarimage"></img>
+        <button className="changeavatar">Change Avatar</button>
+      </div>
+      <div className="userinfo">
+        <div className="usernameinfoholder">
+          <h2 className="label">Username</h2>
+          <h2 className="info" align="left">Sanjee</h2>
+        </div>
+        <div className = "passwordinfoholder">
+          <h2 className="label passwordlabel">Password</h2>
+          <h2 className="info" align="left">***************</h2>
+          <button className="changepassword">Change Password</button>
+        </div>
+        <div className="usernameinfoholder">
+          <h2 className="label">Joined</h2>
+          <h2 className="info" align="left">05/01/2020</h2>
+        </div>
+        <div className="usernameinfoholder">
+          <h2 className="label">Progress</h2>
+          <h2 className="info" align="left">12%</h2>
+        </div>
+        <div className="infobuttons">
+          <button className="infobutton">Back</button>
+          <button className="infobutton">Save</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserInfo;
+
+{ /* <button
         onClick={() => {
           dispatch(changeView("RESET_PAGE"));
         }}
@@ -65,7 +99,7 @@ function UserInfo() {
           Dragon
         </button>
 
-        <button>{/** for test purpose */ avatar}</button>
+        <button>{avatar}</button>
       </div>
       <button>Save</button>
       <button
@@ -75,8 +109,4 @@ function UserInfo() {
       >
         Back
       </button>
-    </div>
-  );
-}
-
-export default UserInfo;
+      </div>  */ }

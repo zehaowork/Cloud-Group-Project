eecfab9e96fcc95fd5efe1bef44d4fd363101b43
Zehao Xue <zehaowork@outlook.com>
2019-12-27 20:09:20 +0000

    Added Loading effect, when logging in
    Imported new package called  ''react-loadingg''
    
    use npm to install it

--- codeplay-app/src/components/Login/Login.jsx
+++ codeplay-app/src/components/Login/Login.jsx
@@ -3,20 +3,27 @@ import "./Login.css";
 import { useDispatch } from "react-redux";
 import { changeView, login } from "../../actions";
 import axios from "axios";
+import { SemipolarLoading } from "react-loadingg";
 
 function Login() {
   const dispatch = useDispatch();
   const [pass, setPass] = useState("");
   const [user, setUser] = useState("");
-
+  const [loading, setLoading] = useState(false);
   /**
    * TODO: Add actual URL for login
    */
   const loginRequest = () => {
-    alert("Your name is " + user + ", Your password is " + pass + ".");
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      dispatch(changeView("GAME_PAGE"));
+      // alert("Your name is " + user + ", Your password is " + pass + ".");
 
-    localStorage.setItem("loginStatus", "ON");
-    localStorage.setItem("user", "xhao98");
+      localStorage.setItem("loginStatus", "ON");
+      localStorage.setItem("user", "xhao98");
+    }, 5000);
+
     // axios
     //   .get("OUR_SEVER")
     //   .then(res => {
@@ -26,47 +33,52 @@ function Login() {
   };
   return (
     <div className="loginpage">
-      <div className="innerlogin">
-        <h1>CODE & PLAY</h1>
-        <input
-          onChange={e => {
-            setUser(e.target.value);
-          }}
-          value={user}
-          type="text"
-          className="logininput"
-          placeholder="Username"
-        ></input>
-        <input
-          onChange={e => {
-            setPass(e.target.value);
-          }}
-          value={pass}
-          type="password"
-          className="logininput"
-          placeholder="Password"
-        ></input>
-        <div className="loginbuttons">
-          <button
-            onClick={() => {
-              loginRequest();
-              dispatch(changeView("GAME_PAGE"));
+      {loading === true && (
+        <SemipolarLoading size="large" speed={3} color="#FFFFFF" />
+      )}
+      {loading === false && (
+        <div className="innerlogin">
+          <h1>CODE & PLAY</h1>
+          <input
+            onChange={e => {
+              setUser(e.target.value);
             }}
-            type="button"
-            className="loginbutton"
-          >
-            Log In
-          </button>
-          <button
-            className="signup"
-            onClick={() => {
-              dispatch(changeView("SIGNUP_PAGE"));
+            value={user}
+            type="text"
+            className="logininput"
+            placeholder="Username"
+          ></input>
+          <input
+            onChange={e => {
+              setPass(e.target.value);
             }}
-          >
-            Sign up?
-          </button>
+            value={pass}
+            type="password"
+            className="logininput"
+            placeholder="Password"
+          ></input>
+
+          <div className="loginbuttons">
+            <button
+              onClick={() => {
+                loginRequest();
+              }}
+              type="button"
+              className="loginbutton"
+            >
+              Log In
+            </button>
+            <button
+              className="signup"
+              onClick={() => {
+                dispatch(changeView("SIGNUP_PAGE"));
+              }}
+            >
+              Sign up?
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
---
+++ data format proposal.js
@@ -0,0 +1,33 @@
+import { login } from "./codeplay-app/src/actions"
+import { bool } from "./codeplay-app/node_modules/@types/prop-types"
+
+
+// load current game page 
+{
+    unit:"NUMBER"， // number/variable etc....
+    story:"dasdasjdka dsajdasidjals ",
+    solution:"1.dsdasds 2.dadsadsad 3. dadsdasdas"
+}
+
+
+// load user profile page
+{
+username:"xhao98",
+progress:20,
+avatar:"WITCH"
+date:"23/09/2019"
+}
+
+
+// login request
+
+function login(user:String,pass:String){
+return bool
+}
+
+function savegame({username:"xhao98",
+progress:20,
+avatar:"WITCH"
+date:"23/09/2019"}){
+    return bool
+}
